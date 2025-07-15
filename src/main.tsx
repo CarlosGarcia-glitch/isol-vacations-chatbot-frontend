c26663d535e5abe4897f2660ac9e474fb60d3694
CarlosGarcia <carlos.garcia@isol.dev>
2025-07-15 14:05:07 -0600

    Add AppContext for authentication and language management, wrapping App component in AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './index.scss';
+import { AppProvider } from './contexts/AppContext';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </StrictMode>,
   );
 }
