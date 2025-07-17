b0f4dce16fcbcbb75d92d0d8d7b23a6057f3f3c6
CarlosGarcia <carlos.garcia@isol.dev>
2025-07-17 15:53:33 -0600

    feat: implement typewriter effect for chatbot messages

--- src/components/ChatbotMessage.tsx
+++ src/components/ChatbotMessage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { useTranslations } from '../contexts/AppContext';
 import ChatbotIcon from './icons/ChatbotIcon';
 
@@ -9,12 +11,34 @@ export interface IChat {
 const ChatbotMessage = ({ role, message }: IChat) => {
   const t = useTranslations();
 
+  const fullText: string = message === 'processing' ? t.processing : message;
+  const element = useRef<HTMLDivElement>(null);
+  let index: number = 0;
+
+  useEffect(() => {
+    function typeWriter() {
+      if (role === 'bot') {
+        if (index < fullText.length) {
+          if (element?.current) {
+            element.current.textContent += fullText.charAt(index);
+          }
+          index++;
+          setTimeout(typeWriter, 20);
+        }
+      } else {
+        if (element?.current) {
+          element.current.textContent = fullText;
+        }
+      }
+    }
+
+    typeWriter();
+  }, []);
+
   return (
     <div className={`message ${role}-message`}>
       {role === 'bot' && <ChatbotIcon />}
-      <p className="message-text">
-        {message === 'processing' ? t.processing : message}
-      </p>
+      <p ref={element} className="message-text" />
     </div>
   );
 };
