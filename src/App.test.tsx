fea7979e790780a23fc7cdc0f222284fe5ae523e
CarlosGarcia <carlos.garcia@isol.dev>
2025-07-22 10:41:07 -0600

    test: add unit tests for App component and include password input data-testid

---
+++ src/App.test.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from '@mui/material';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import theme from './assets/theme';
+import { AppProvider } from './contexts/AppContext';
+import { RouterProvider } from './providers/RouteProvider';
+
+describe('App', () => {
+  it('renders the App component', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <AppProvider>
+          <RouterProvider />
+        </AppProvider>
+      </ThemeProvider>,
+    );
+    
+    expect(
+      screen.getByRole('heading', { level: 3, name: 'Bienvenidos a ISOL' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('textbox', {name: /email/i})).toBeInTheDocument()
+    expect(screen.getByTestId('password-input')).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Iniciar sesión' }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText('© 2025 by ISOL. All rights reserved'),
+    ).toBeInTheDocument();
+  });
+});
