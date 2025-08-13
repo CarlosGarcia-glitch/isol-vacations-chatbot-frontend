2ad26c73599462896836d184021071c7ddbad67b
CarlosGarcia <carlos.garcia@isol.dev>
2025-08-12 21:04:00 -0600

    feat: enhance login page with description and refactor login handling to use Google authentication

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,130 +1,80 @@
-import { useState } from 'react';
+import { Button, CircularProgress } from '@mui/material';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FormikValues, useFormik } from 'formik';
-import {
-  Button,
-  CircularProgress,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  TextField,
-} from '@mui/material';
-import { VisibilityOffOutlined, VisibilityOutlined } from '@mui/icons-material';
 
 import Footer from '@/components/Footer/Footer';
-import { getLoginFormSchema } from '@/utils/schemas/loginFormSchema';
 
-import Styles from './_Login.module.scss';
-import { useAlert, useAppContext, useTranslations } from '@/contexts/AppContext';
+import { GoogleIcon } from '@/components/icons/GoogleIcon';
+import {
+  useAlert,
+  useAppContext,
+  useTranslations,
+} from '@/contexts/AppContext';
 import AuthService from '@/services/authService';
+import Styles from './_Login.module.scss';
 
-type Props = {};
-
-const Login = (props: Props) => {
+const Login = (props: {}) => {
   const t = useTranslations();
   const { setUser } = useAppContext();
   const navigate = useNavigate();
   const { setAlert } = useAlert();
 
   const [loading, setLoading] = useState<boolean>();
-  const [showPassword, setShowPassword] = useState<boolean>();
 
-  const schema = getLoginFormSchema(t);
+  const checkUserSession = useCallback(async () => {
+    try {
+      const userData = await AuthService.getCurrentUser();
+      setUser(userData);
+      localStorage.setItem('token', userData?.access_token);
+      setAlert(true, 'success', t.login.alerts.success.login);
+      navigate('/chat');
+    } catch (error) {
+      setUser(null);
+      setAlert(true, 'error', 'No active session found.');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-  const { errors, touched, values, handleBlur, handleChange, handleSubmit } =
-    useFormik({
-      initialValues: { email: '', password: '' },
-      enableReinitialize: true,
-      validationSchema: schema,
-      onSubmit: async (vals: FormikValues) => {
-        setLoading(true);
-        try {
-          const resp = await AuthService.login(vals.email, vals.password);
-          console.log('resp', resp);
-          setUser(resp?.user_info);
-          localStorage.setItem('token', resp?.access_token);
-          setAlert(true, 'success', t.login.alerts.success.login);
-          navigate('/chat');
-        } catch (error) {
-          console.error(error);
-          setAlert(true, 'error', t.login.alerts.error.login);
-          setLoading(false);
+  const handleSubmit = useCallback(async () => {
+    setLoading(true);
+    try {
+      const { auth_url } = await AuthService.getLoginUrl();
+      const popup = window.open(
+        auth_url,
+        'google-login',
+        'width=500,height=600,noopener,noreferrer',
+      );
+
+      const timer = setInterval(async () => {
+        if (!popup || popup.closed) {
+          clearInterval(timer);
+          await checkUserSession();
         }
-      },
-    });
+      }, 500);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+      setLoading(false);
+    }
+  }, [checkUserSession]);
 
   return (
     <div className={Styles.page__layout}>
       <div className={Styles.page__content}>
         <div className={Styles.login__card}>
-          <div className={Styles.login__card_img}>
-            {/* <img src={Logo} alt="" /> */}
-          </div>
+          <div className={Styles.login__card_img}></div>
           <div className={Styles.login__card_body}>
             <div className={Styles.login__card_title}>
               <h3>{t.login.labels.title}</h3>
             </div>
-            <InputLabel htmlFor="email">{t.login.labels.email}</InputLabel>
-            <TextField
-              id="email"
-              name="email"
-              placeholder="example@isol.com"
-              value={values.email}
-              error={touched?.email && Boolean(errors.email)}
-              helperText={
-                touched.email && typeof errors.email === 'string'
-                  ? errors.email
-                  : undefined
-              }
-              onBlur={handleBlur}
-              onChange={handleChange}
-              onKeyDown={(e) => e.key === 'Enter' && handleSubmit()}
-            />
-            <InputLabel htmlFor="password">
-              {t.login.labels.password}
-            </InputLabel>
-            <TextField
-              data-testid="password-input"
-              id="password"
-              name="password"
-              placeholder={t.login.labels.password}
-              type={showPassword ? 'text' : 'password'}
-              value={values.password}
-              error={touched?.password && Boolean(errors.password)}
-              helperText={
-                touched.password && typeof errors.password === 'string'
-                  ? errors.password
-                  : undefined
-              }
-              onBlur={handleBlur}
-              onChange={handleChange}
-              onKeyDown={(e) => e.key === 'Enter' && handleSubmit()}
-              slotProps={{
-                input: {
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword(!showPassword)}
-                        onMouseDown={(e) => e.preventDefault()}
-                        edge="end"
-                      >
-                        {showPassword ? (
-                          <VisibilityOffOutlined />
-                        ) : (
-                          <VisibilityOutlined />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                },
-              }}
-            />
+            <p className={Styles.login__card_description}>
+              {t.login.labels.description}
+            </p>
           </div>
           <div className={Styles.login__card_footer}>
             <Button
               disabled={loading}
-              variant="contained"
+              variant="outlined"
               color="primary"
               startIcon={
                 loading ? (
@@ -134,8 +84,10 @@ const Login = (props: Props) => {
                   />
                 ) : undefined
               }
-              onClick={() => handleSubmit()}
+              onClick={handleSubmit}
             >
+              {!loading && <GoogleIcon />}
+              {'  '}
               {loading ? t.login.labels.signingIn : t.login.labels.signIn}
             </Button>
           </div>
